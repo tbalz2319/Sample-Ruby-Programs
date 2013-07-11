5fb7982983ca06f3e096f9edc49610f43d9f2267
talal234 <tbalouch@gmail.com>
2013-07-11 11:41:34 -0400

    Update Hello.rb

--- Hello.rb
+++ Hello.rb
@@ -1,10 +1,22 @@
 class Hello
-  #Initializing the name constructor
+	#Initializing the name constructor
 	def initialize(n)
 		@name = n
 	end
     def namez
         print "Your name is " + @name
+    end
+    #getter or accessor method for @name variables
+    def getName
+      @name
+    end
+    def setName=(value)
+      @name = value
+    end
+    def to_s
+      #Any class you define should have a to_s instance method to return
+      #a string representation of the object.
+      "(name:#{@name})" #String representation of the object Hello
     end
  end
 
@@ -17,3 +29,22 @@ class Hello
   #Hello to Ralph
   talal = Hello.new('Ralph')
   puts talal.namez()
+  
+  #Displays output of talal object showing name ralph  
+  puts talal.getName()
+  #Displays output of greeting object showing name bob
+  puts greeting.getName
+
+# use of setter method to change ralph to Polo name
+  talal.setName = 'Polo'
+  #will show polo
+  puts talal.getName()
+  #This will make a string representation of 
+  puts "String representation of talal is : #{talal}"
+
+#Created new object drevon and will show a string representation of it
+drevon = Hello.new('Drevon Butler')
+puts "String representation of Drevon is : #{drevon}"
+
+newvar = talal.getName
+print "The value of for talal object is #{newvar}"
