eed0395bfe2a3afdfb5c06fcae030edd218cd3f4
talal234 <tbalouch@gmail.com>
2013-07-17 15:34:03 -0400

    Create forloop.rb

---
+++ forloop.rb
@@ -0,0 +1,8 @@
+#Example of a for loop initialized at 0 and interating until it hits 5 and printing the
+#value of the variable 
+talal = 0
+for talal in 0..10
+  #counter below will keep incrementing talal variable by 1 for example talal = talal + 1
+    talal += 1
+    puts " #{talal}"
+end
