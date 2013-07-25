67c6b1adaa2eb3270970cf2024c8fb59a6e34108
talal234 <tbalouch@gmail.com>
2013-07-25 16:19:37 -0400

    Update mortgage.rb

--- mortgage.rb
+++ mortgage.rb
@@ -1,27 +1,51 @@
-#print "Enter Loan amount: "
-#loan = gets.chomp.to_i
-loan = 425000
-#print "Enter length of time in months: "
-#time = gets.chomp.to_i
-time = 360
-time2 = 180
-#print "Enter interest rate: "
-#rate = gets.chomp.to_f/100
-rate = 4.0/100
+#Object Oriented Mortgage Calculator
+class MortgageCalculator
+  #Constructor to initialize all variables used for
+	#Mortgage calculation
+	def initialize(loan, time, rate)
+		@loan = loan
+		@time = time
+		@rate = rate
+		@cRate = rate/100
+		@j = (1+@cRate/12)**(12/12)-1
+		@annuity = (1-(1/(1+@j))**time)/@j
+		@monthly = self.payment
+		@interest_number = self.converted
+		@super_con = self.converted
+	end
+	def payment
+		@loan  / @annuity
+	end
+	def converted
+		@cRate * 100
+	end
+	def showVariables
+		@monthly = self.payment
+		@super_con = self.converted
+		puts "The monthly cost for this home is $%.2f" % [monthly]
+		puts "This is based off a #{@interest_number} % rate"
+		puts "The loan amount is for #{@loan}"
+				if @time == 360
+					puts "The loan is for 30 years"
+				else
+					puts "The loan is for 15 years"
+				end
+	end
+	#accessor methods so instance variables can be accessed
+	#outside of the class
+	attr_accessor :loan, :time, :rate, :cRate, :j, :monthly
+end
 
-i = (1+rate/12)**(12/12)-1
+loan1 = MortgageCalculator.new(425000, 360, 4.0)
+loan2 = MortgageCalculator.new(550000, 180, 4.0)
+loan3 = MortgageCalculator.new(1000000, 360, 3.2)
+loan3.time = 360
+loan3.loan = 45000
 
-annuity = (1-(1/(1+i))**time)/i
-payment = loan / annuity
+#monthly_cost = loan1.payment
+#puts "\nThe monstly cost for this loan is $%.2f" % [monthly_cost]
 
-annuity2 = (1-(1/(1+i))**time2)/i
-payment2 = loan / annuity2
-
-convertz = rate * 100
-
-puts "\n$%.2f per month" % [payment]
-puts "For a Loan amout of $#{loan}"
-puts "For a period of #{time} months or 30 years"
-puts "For a period of #{time2} months or 15 years its will be: "
-puts "\n$%.3f per month for 15 years" % [payment2]
-puts "For an interest rate of #{rate} or #{convertz} percent"
+puts loan1.showVariables
+puts loan2.showVariables
+puts loan3.showVariables
+puts loan3.loan
